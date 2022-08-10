c31b9693171db15dfd8d9dd064a1a2f000748675
amahdevar <abolfazlmahdevar@gmail.com>
2022-08-10 18:27:51 +0430

    css inital done - worked on eventListeners

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 let inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function(){
+    console.log("Button Clicked")
+})
