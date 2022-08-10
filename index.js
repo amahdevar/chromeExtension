b9e453d0fd165d0adc6c57f062b544837f724bef
amahdevar <abolfazlmahdevar@gmail.com>
2022-08-10 22:10:27 +0430

    Basic work on html css and js has been done
    css for buttons and list has been done
    html pulished and ids have been added
    js have inital fucntions

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-let inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
-
-inputBtn.addEventListener("click", function(){
-    console.log("Button Clicked")
-})
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEL = document.getElementById("ul-el");
+let myLead =[];
+//Push input text into array
+inputBtn.addEventListener("click", function() {
+    myLead.push(inputEl.value);
+    inputEl.value = ""
+    renderLeads();
+}
+);
+function renderLeads(){
+let listItems = "";
+for (let i = 0; i < myLead.length; i++){
+    listItems += `
+    <li>
+    <a target='_blank' href='//${myLead[i]}'>
+    ${myLead[i]}
+    </a>
+    </li>
+    `
+};
+ulEL.innerHTML = listItems;
+}
+// Video link: `https://youtu.be/jS4aFq5-91M?t=19531
