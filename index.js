61befe0890b55ec169606da4e8cfcbb5c8e708e7
amahdevar <abolfazlmahdevar@gmail.com>
2022-08-11 14:55:26 +0430

    added localstorage to list, added delete for list

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEL = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 let myLead =[];
-//Push input text into array
-inputBtn.addEventListener("click", function() {
-    myLead.push(inputEl.value);
-    inputEl.value = ""
-    localStorage.setItem("mylead", JSON.stringify(myLead))
-    renderLeads();
-    console.log(localStorage.getItem("mylead"))
+let leadFromLocalStorage = JSON.parse(localStorage.getItem("mylead"));
+if (leadFromLocalStorage) {
+    myLead = leadFromLocalStorage;
+    render(myLead);
 }
-);
-function renderLeads(){
-let listItems = "";
-for (let i = 0; i < myLead.length; i++){
-    listItems += `
-    <li>
-    <a target='_blank' href='//${myLead[i]}'>
-    ${myLead[i]}
-    </a>
-    </li>
-    `
-};
+function render(leads){
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++){
+        listItems += `
+        <li>
+        <a target='_blank' href='//${leads[i]}'>
+        ${leads[i]}
+        </a>
+        </li>
+        `
+    }
 ulEL.innerHTML = listItems;
 }
+deleteBtn.addEventListener("dblclick", function(){
+   localStorage.setItem("mylead", null);
+   myLead = [];
+   render(myLead);
+})
+inputBtn.addEventListener("click", function() {
+    myLead.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("mylead", JSON.stringify(myLead));
+    render(myLead);
+}
+)
+user = {
+    name: "ali",
+    age: 22
+}
+
+
 // Video link: https://youtu.be/jS4aFq5-91M?t=22269
